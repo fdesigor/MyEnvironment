29ee08366f0a98b2d8a671a91656af01c23ed830
Igor Santos <igorfs042@gmail.com>
2019-04-18 16:07:17 -0300

    Consolidação de pacotes a serem instalados pelo install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo -e "\e[01;32m"
 echo ==============================================
-echo "# Atualizando repositórios..."
+echo "# Atualizando repositórios"
 echo ==============================================
 echo -e "\e[00m"
 
@@ -10,6 +10,7 @@ if ! apt update
 then
   echo -e "\e[00;31m"
   echo "Não foi possível atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list"
+  echo -----------------------------------------------------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
@@ -18,7 +19,7 @@ apt list --upgradable
 
 echo -e "\e[01;32m"
 echo ==============================================
-echo "# Atualizando pacotes já instalados..."
+echo "# Atualizando pacotes já instalados"
 echo ==============================================
 echo -e "\e[00m"
 
@@ -26,6 +27,7 @@ if ! apt dist-upgrade -y
 then
   echo -e "\e[00;31m"
   echo "Não foi possível atualizar pacotes."
+  echo ----------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
@@ -33,71 +35,109 @@ fi
 
 echo -e "\e[01;32m"
 echo ==============================================
-echo "# Instalando ferramentas..."
+echo "# Instalando ferramentas"
 echo ==============================================
+echo -e "\e[00m"
+
+echo -e "\e[01;32m"
+echo "# Instalando o build-essential"
+echo ----------------------------------------------
+echo -e "\e[00m"
+apt install build-essential -y
+
+echo -e "\e[01;32m"
+echo "# Instalando o git"
+echo ----------------------------------------------
 echo -e "\e[00m"
 if ! apt install git -y
 then
   echo -e "\e[00;31m"
   echo "Não foi possível instalar o git"
+  echo ----------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
 
+echo -e "\e[01;32m"
+echo "# Instalando o vim"
+echo ----------------------------------------------
+echo -e "\e[00m"
 if ! apt install vim -y
 then
   echo -e "\e[00;31m"
   echo "Não foi possível instalar o vim"
+  echo ----------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
 
+echo -e "\e[01;32m"
+echo "# Instalando o zsh"
+echo ----------------------------------------------
+echo -e "\e[00m"
 if ! apt install zsh -y
 then
   echo -e "\e[00;31m"
   echo "Não foi possível instalar o zsh"
+  echo ----------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
 
+echo -e "\e[01;32m"
+echo "# Instalando o tmux"
+echo ----------------------------------------------
+echo -e "\e[00m"
 if ! apt install tmux -y
 then
   echo -e "\e[00;31m"
   echo "Não foi possível instalar o tmux"
+  echo ----------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
 
-if ! apt install zeal -y
+echo -e "\e[01;32m"
+echo "# Instalando o python3"
+echo ----------------------------------------------
+echo -e "\e[00m"
+if ! apt install python3 -y
 then
   echo -e "\e[00;31m"
-  echo "Não foi possível instalar o zeal"
+  echo "Não foi possível instalar o python3"
+  echo ----------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
 
-if ! apt install build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget -y
-then
-  echo -e "\e[00;31m"
-  echo "Não foi possível instalar"
-  echo -e "\e[00m"
-  exit 1
-fi
-
-curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-
+echo -e "\e[01;32m"
+echo "# Instalando o nodejs"
+echo ----------------------------------------------
+echo -e "\e[00m"
 if ! apt install nodejs -y
 then
   echo -e "\e[00;31m"
   echo "Não foi possível instalar o nodejs"
+  echo ----------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
 
+echo -e "\e[01;32m"
+echo "# Atualizando o npm"
+echo ----------------------------------------------
+echo -e "\e[00m"
+npm install -g npm
+
+echo -e "\e[01;32m"
+echo "# Instalando o docker"
+echo ----------------------------------------------
+echo -e "\e[00m"
 if ! apt install docker -y
 then
   echo -e "\e[00;31m"
   echo "Não foi possível instalar o docker"
+  echo ----------------------------------------------
   echo -e "\e[00m"
   exit 1
 fi
